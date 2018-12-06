63e81fcdc8be7f7266d0724373fecb256d6ec5cc
Kishan Rathore <rkishan516@gmail.com>
2018-12-06 10:33:31 +0530

    UI Updated

--- popup.js
+++ popup.js
@@ -6,6 +6,87 @@ var save_btns=[];
 var remove_btns=[];
 var load_btns=[];
 
+var count = 0;
+var container = document.getElementsByClassName('container');
+var listGroupItem = document.getElementsByClassName('list-group-item');
+var panelHeading = document.getElementsByClassName('panel-heading');
+var panelTitle = document.getElementsByClassName('panel-title');
+var panel = document.getElementsByClassName('panel');
+var tab_content = document.getElementsByClassName('tab-content');
+var navTab = document.getElementsByClassName('nav-tabs');
+var active = document.getElementsByClassName('active');
+var i;
+document.addEventListener('DOMContentLoaded', function() {
+    var theme = document.getElementById('theme');
+    theme.addEventListener('click', function() {
+        changeTheme();
+    });
+});
+
+function changeTheme(){
+    if(count){
+        for (i = 0; i < active.length; i++) {
+            active[i].style.backgroundColor = "#424242";
+        }
+        for (i = 0; i < listGroupItem.length; i++) {
+            listGroupItem[i].style.backgroundColor = "#424242";
+            listGroupItem[i].style.color = '#fff';
+        }
+        for (i = 0; i < panelHeading.length; i++) {
+            panelHeading[i].style.backgroundColor = "#424242";
+        }
+        for (i = 0; i < panelTitle.length; i++) {
+            panelTitle[i].style.backgroundColor = "#424242";
+            panelTitle[i].style.color = "white";
+        }
+        for (i = 0; i < panel.length; i++) {
+            panel[i].style.backgroundColor = "#424242";
+        }
+        for (i = 0; i < navTab.length; i++) {
+            navTab[i].style.color = "#fff";
+            navTab[i].style.backgroundColor = "#212121";
+            navTab[i].style.border = "1px solid #424242";
+        }
+        for (i = 0; i < tab_content.length; i++) {
+            tab_content[i].style.backgroundColor = "#fff";
+        }
+        for (i = 0; i < container.length; i++) {
+            container[i].style.backgroundColor = "#212121";
+        }
+        count = 0;
+    }else{
+        for (i = 0; i < active.length; i++) {
+            active[i].style.backgroundColor = "#f5f5f5";
+        }
+        for (i = 0; i < listGroupItem.length; i++) {
+            listGroupItem[i].style.backgroundColor = "#f5f5f5";
+            listGroupItem[i].style.color = '#000';
+        }
+        for (i = 0; i < panelHeading.length; i++) {
+            panelHeading[i].style.backgroundColor = "#f5f5f5";
+        }
+        for (i = 0; i < panelTitle.length; i++) {
+            panelTitle[i].style.backgroundColor = "#f5f5f5";
+            panelTitle[i].style.color = "black";
+        }
+        for (i = 0; i < panel.length; i++) {
+            panel[i].style.backgroundColor = "#fff";
+        }
+        for (i = 0; i < navTab.length; i++) {
+            navTab[i].style.color = "#555";
+            navTab[i].style.backgroundColor = "#fff";
+            navTab[i].style.border = "1px solid #ddd";
+        }
+        for (i = 0; i < tab_content.length; i++) {
+            tab_content[i].style.backgroundColor = "#fff";
+        }
+        for (i = 0; i < container.length; i++) {
+            container[i].style.backgroundColor = "#f5f5f5";
+        }
+        count = 1;
+    }
+}
+
 var x= function() 
 {
     chrome.windows.getAll(function(win_all)
