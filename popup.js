a25a4f8999b0bb1b8c72b3c483ff61a79f166362
rkishan516 <rkishan516@gmail.com>
2018-12-31 15:19:21 +0530

    Updated UI for Saved Windows and Removed unused variables

--- popup.js
+++ popup.js
@@ -28,7 +28,6 @@ var current_tab_data = () => {
     var no_of_windows=single_window.length;
 
     for(i=0;i<no_of_windows;i++) {
-        var second_tab = 0;
         str1+='<div class="panel panel-default" id="window_'+(i+1)+'" >'+
         '<div class="panel-heading">'+
         '<h4 class="panel-title">'+
@@ -69,7 +68,6 @@ var saved_tab_data =  () => {
     }
     str4='';
     for(l=0;l<total_windows;l++) {
-        var second_tab = 0;
         str4+='<div class="panel panel-default">'+
         '<div class="panel-heading">'+
         '<h4 class="panel-title">'+
@@ -95,19 +93,10 @@ var saved_tab_data =  () => {
 
             // workaround
             if (f == 'chrome://theme/IDR_EXTENSIONS_FAVICON') f = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVQoz2P4z4AfMpCgQMfI6LbBZi0mnArCK73f+jyOZMWiQENOU0KHe3N01KuOg/+ZNHg0ZNSZUBSYn7T8aP3B6230y5DX9u8tP5p+suZDURD6IOJV1Mu4FynPE19Ev4x85fvmPweKgrj76c8ykWDkKwtOTVUNHriC5atfLNy2Nf05TEHMS9d9Fh9M98IVvLb5z7c5IOEFTEHy88yrIa+DH8AVmDwy7/BeEwdXkPrsf+3aHXsXwhX4P/V7E/I6+8HpNUCLgAqiX7qw/rf+rwZXkHEj8UXii5aj/1Xv26c+yXwW8SpIAsUX1fvz72c/ir38n8EtO/Fx1Y2ai63yqEEd97/zf///eiDL8X/f/9r/Wf95iI5NALw5DuHmTOHfAAAAAElFTkSuQmCC';
-            if(second_tab==3){
-                str6= '<td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td></tr>';
-                second_tab= 0;
-            }else if(second_tab == 0){
-                str6 = '<tr><td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td>';
-                second_tab++;
-            }else{
-                str6 = '<td><div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div></td>';
-                second_tab++;
-            }
+            str6 = '<div class="list-group-item" id="item">'+'<div class="circle"><img src='+f+' /></div> <div id="text"><center>'+t+'</center></div></div>';
             str5+=str6;
         }
-        str4+=str5+'</table>'+'</div>'+'</div>'+'</div>';
+        str4+=str5+'</div>'+'</div>'+'</div>';
     }
     document.getElementById('saved').innerHTML=str4;
 };
